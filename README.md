c05b286f30e46684176dde899b79b964f34f6038
Arinal Dzikrul <75413022+arinaldha@users.noreply.github.com>
2024-11-01 20:43:23 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# anagrams
+# Installation
+
+1. clone with command `git clone https://github.com/arinaldha/anagrams.git`
+2. go to the project with command `cd anagrams`
+3. run command `node main.js`
---
+++ main.js
@@ -0,0 +1,32 @@
+function groupAnagrams(words) {
+  const anagramMap = new Map();
+
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+
+    if (anagramMap.has(sortedWord)) {
+      anagramMap.get(sortedWord).push(word);
+    } else {
+      anagramMap.set(sortedWord, [word]);
+    }
+  }
+
+  const result = Array.from(anagramMap.values())
+    .map((group) => group.sort())
+    .sort((a, b) => b.length - a.length || a[0].localeCompare(b[0]));
+
+  return result;
+}
+
+const words = [
+  "kita",
+  "atik",
+  "tika",
+  "suka",
+  "aku",
+  "kia",
+  "kaus",
+  "makan",
+  "kua",
+];
+console.log(groupAnagrams(words));
